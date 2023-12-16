50adf707b28c4ea2cf86e1117ba9454102dd7837
Amin <m.aminwhat@gmail.com>
2023-12-17 01:48:56 +0330

    Just Vazir

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import { Landing } from "@/components/component/landing";
-
 export default function Home() {
-  return Landing();
+  return (
+    <center>
+      <h1>Vazir Inc.</h1>
+    </center>
+  );
 }
